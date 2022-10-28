5024ce51b6e96ae98ae3fa72da6851d3de5f3023
Sanja <sanja1mandic@gmail.com>
2022-10-28 09:39:33 +0200

    Add functionality for SignIn and Sign up, to post data to API, and check if user exist ot to create userand to redirect to home page

--- src/redux/user.js
+++ src/redux/user.js
@@ -1,12 +1,16 @@
-const url = 'http://127.0.0.1:4000/users'
-const POSTUSER = 'POSTUSER';
+const url1 = 'http://127.0.0.1:3000/users'
+const url2 = 'http://127.0.0.1:3000/auth/login'
+const POST_USER = 'POST_USER';
+const AUTH_USER = 'AUTH_USER';
 const initialState = {};
 
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case POSTUSER:
-      return action.user;
+    case POST_USER:
+      return action.username;
+    case AUTH_USER:
+      return action.username;
     default:
       return state;
   }
@@ -15,22 +19,43 @@ const userReducer = (state = initialState, action) => {
 export default userReducer;
 
 export const saveUser = (user) => async(dispatch) => {
-  fetch(url,
+  const response = await fetch(url1,
     {
       method: 'POST',
       body: JSON.stringify({
         username: user.name,
+        email: user.email,
         password: user.password,
         password_confirmation: user.passwordConf
       }),
       headers: {
-        Accept: "application/json",
-                "Content-Type": "application/json"
+       "Content-Type": "application/json"
       },
     });
+    const userData = await response.json();
+  dispatch({
+    type: POST_USER,
+    userData
+  })
+}
+
+export const authUser = (user) => async(dispatch) => {
+  const response = await fetch(url2,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email: user.email,
+        password: user.password,
+      }),
+      headers: {
+       "Content-Type": "application/json"
+      },
+    });
+  const username = await response.json();
+
 
   dispatch({
-    type: POSTUSER,
-    user,
+    type: AUTH_USER,
+    username
   })
 }
