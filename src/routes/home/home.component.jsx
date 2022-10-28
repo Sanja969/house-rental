da3776ad7d16735ec737fb79c13a65e9e76d10e9
Sanja <sanja1mandic@gmail.com>
2022-10-28 09:40:21 +0200

    Deleted unesesery outline component

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
       <h1>This is home</h1>
-      <Outlet />
     </div>
 
   );
