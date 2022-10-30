22daadb52aa995849a725e89d386a7555b1686d9
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-10-31 00:11:41 +0300

    Add action creater and url for houses

---
+++ src/redux/home.js
@@ -0,0 +1,10 @@
+const url = "http://127.0.0.1:3000/houses"
+
+const GET_HOUSES = "GET_HOUSES"
+
+const initialState = []
+
+
+
+// Path: src/redux/store.js
+// Compare this snippet from src/redux/store.js:
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import {getHouses} from '../../redux/home'
+
+// const house = useSelector(state => state);
+// console.log(house);
+
 const Home = () => {
+ 
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getHouses());
+  }, []);
+
+  const house = useSelector((state) => state.house);
+  console.log(house);
   return (
     <div>
       <h1>This is home</h1>
