8563ae62105ed86d8063b0d6412659f8356541d9
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-10-31 10:19:40 +0300

    Show only three houses to home page

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,25 +3,30 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import {getHouses} from '../../redux/home'
 import HomePage from '../../components/homepage/home-page';
+import './home.css';
 
 
 const Home = () => {
- 
+ let n = 3;
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getHouses());
   }, []);
 
   const house = useSelector((state) => state.house);
-  console.log(house);
+
+  const tophouses = house.filter((house) => house.id <= n);
+  console.log(tophouses);
+
   return (
     <div>
       <h1>List of Houses</h1>
       <div className="all-houses">
-       
+       <button>back</button> 
       {
-      house.map((item) => (<HomePage key={item.id} List={item} />))
+      tophouses.map((item) => (<HomePage key={item.id} List={item} />))
       }
+      <button>forward</button>
       </div>
     </div>
 
