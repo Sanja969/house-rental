45a02bfc39bb97d3d63225ae8aa74889f9f52f9d
Sanja <sanja1mandic@gmail.com>
2022-11-01 23:54:43 +0100

    Delete 'X' button and add instead of it left arrow for hiding navbar, and change style to fit it

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,41 +1,47 @@
 import React, {useState} from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
 import brand from '../../logo.png';
 import './navigation.styles.scss';
 import { SidebarData } from './SidebarData';
+import { AiFillCaretLeft } from "react-icons/ai";
 
 const Navigation = () => {
   const [sidebar, setSidebar]  = useState(false)
-  const showSidebar = () => setSidebar(!sidebar);
+  const triggleSidebar = () => setSidebar(!sidebar);
   return (
     <>
       <div className='navbar'>
         <Link to="#" className='menu-bars ham' >
-          <FaIcons.FaBars onClick={showSidebar} />
+          <FaIcons.FaBars onClick={triggleSidebar} />
         </Link>
+        <h2>House rental</h2>
       </div>
       <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-        <ul className='nav-menu-items' onClick={showSidebar}>
+        <ul className='nav-menu-items'>
           <li className="navbar-toggle">
             <Link to='#' className='menu-bars logo-container'>
-                <AiIcons.AiOutlineClose /> 
                 <img className="logo" src={brand} alt="house" width={30} height={30} />
                 <h2>House rental</h2>
             </Link> 
           </li>
-          {SidebarData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-              )
-          })}
+          <div className='nav-body'>
+            <div className='links'>
+              {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path} onClick={triggleSidebar}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+                )
+              })}
+            </div>
+            <AiFillCaretLeft className='hide-nav' onClick={triggleSidebar}/>
+          </div>
         </ul>
+        
       </nav>
         <Outlet />
     </>
