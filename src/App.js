1af2c67f24cee8e4fdb48cc09611ac27b23c66d0
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-11-02 13:38:47 +0300

    Add routes reservation form

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Navigation from './routes/navigation/navigation.component.jsx';
 import Home from './routes/home/home.component.jsx';
 import HomeDetail from './routes/home-detail/homedetails.jsx';
 import Authentication from './routes/authentication/authentication.component';
+import ReservationForm from './components/reservation-form/reservation';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Navigation />}>
       <Route index element={<Home />} />
       <Route path="house" element={<HomeDetail />} />
+      <Route path="reserve" element={<ReservationForm />} />
       <Route path="auth" element={<Authentication />} />
     </Route>
   </Routes>
