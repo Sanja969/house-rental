4a39ca872753e8791beb9b2cba1d4442554775bf
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-11-02 13:35:32 +0300

    Reservation form setup

---
+++ src/components/reservation-form/reservation.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ReservationForm() {
+
+    return (
+        <div>
+            <h1>Reservation Form</h1>
+        </div>
+    )
+}
--- src/routes/home-detail/homedetails.jsx
+++ src/routes/home-detail/homedetails.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 import { useSelector} from 'react-redux';
 import './homedetails.css';
 import {  AiFillCaretLeft } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 export default function HomeDetail () {
 
     const house = useSelector((state) => state.house);
     console.log('house', house)
     return (
-        <div>
-            <div className="">
-               <h2>{house.name}</h2>
+        <div className='home-detail'>
+            <div className="details-item">
                <img className='image-detail' src={house.image_data} alt={house.name} />
-            </div>
-            <div>
-               <div className="back-arrow">
-                  < AiFillCaretLeft />
+               <div className='detail-about'>
+                   <h2>{house.name}</h2> 
+                    <p>{house.description}</p>
                </div>
+            </div>
+            <div className="">
+               <Link to='/' >
+                  < AiFillCaretLeft />
+               </Link>
                <button>Reserve</button>
             </div>
         </div>
