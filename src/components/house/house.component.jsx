bb2a89994132052da5c8f8baa660de9b00558930
Sanja <sanja1mandic@gmail.com>
2022-11-03 09:47:08 +0100

    Add left margin to home page when navbar is showen and redirect to auth page when user is sigout

--- src/components/house/house.component.jsx
+++ src/components/house/house.component.jsx
@@ -1,23 +1,26 @@
 import React from "react"
 import { PropTypes } from 'prop-types';
 import './house.styles.scss';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
-const House = ({ house }) =>
-{
+const House = ({ house }) => {
   const { image_data, name, id, description} = house;
+  const user = useSelector(state => state.user);
   return (
-    <div  className="house-box">
-      <Link to='/house' id={id}>
-     <img className="home-image" src={image_data} alt={name} />
-      </Link>
-     <h3 className="home-name">{name}</h3>
-     <p className="home-description">{description}</p>
-     <div className="home-links">
-        <Link to='/house' className="detail-link">Detail</Link>
-        <Link to="/reserve" className="reserve">Reserve</Link>
-     </div>
-    </div>
+    <>
+      {!user.username ? <Navigate to='/auth' /> : <div className="house-box">
+        <Link to='/house' id={id}>
+          <img className="home-image" src={image_data} alt={name} />
+        </Link>
+        <h3 className="home-name">{name}</h3>
+        <p className="home-description">{description}</p>
+        <div className="home-links">
+          <Link to='/house' className="detail-link">Detail</Link>
+          <Link to="/reserve" className="reserve">Reserve</Link>
+        </div>
+        </div>}
+    </>
   )
 }
 
