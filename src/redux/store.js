ad3928c0d4c502021b658d48ea2bbab4d598c54b
Sanja <sanja1mandic@gmail.com>
2022-11-03 09:40:16 +0100

    Add reservatin and navbar reducers in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user';
 import  homeReducer from './home';
+import reservationsReducer from './reservations';
+import navbarReducer from './navbar';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    house: homeReducer,
+    houses: homeReducer,
+    reservations: reservationsReducer,
+    navbar: navbarReducer
   },
 });
