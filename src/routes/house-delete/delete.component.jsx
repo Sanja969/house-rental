94db9115a1e4fbd52e661a2173c147c630a31265
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-11-04 08:47:20 +0300

    Add delete house css

--- src/routes/house-delete/delete.component.jsx
+++ src/routes/house-delete/delete.component.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import './delete.styles.scss'
 
-export default function HouseDelete () {
+ const HouseDelete = () => {
+
+    const house = useSelector(state => state.houses);
+    console.log(house);
     return (
         <div>
-            <h1>Are you sure you want to delete this house?</h1>
+            {
+                house.map((house, index) => {                 
+                    return (
+                        <div className='delete-container' key={index}>
+                            <img src={house.image_data} alt={house.name} />
+                            <div className='house-data'>
+                            <h1>{house.name}</h1>
+                            <p>{house.description}</p>
+                            <p>{house.address}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
+
+export default HouseDelete;
