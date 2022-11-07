e0787c53c1df1ce93ad024e294fea530b2fa9f42
Sanja <sanja1mandic@gmail.com>
2022-11-07 20:44:28 +0100

    Fix bugs with delete component functionality

--- src/routes/house-delete/delete.component.jsx
+++ src/routes/house-delete/delete.component.jsx
@@ -1,59 +1,39 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { useState } from 'react';
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import './delete.styles.scss'
 import DeletePopUp from './delete.confirmation'; 
-
+import { getHouse } from '../../redux/detail';
 
 export default function  HouseDelete () {
 
-    const house = useSelector(state => state.houses);
-    const navbar = useSelector(state => state.navbar);
-    const [trigger, setTrigger] = useState(false);
-    const [selectedhouse, setSelectedHouse] = useState(0);
-  
+  const houses = useSelector(state => state.houses);
+  const navbar = useSelector(state => state.navbar);
+  const dispatch = useDispatch();
+  const [trigger, setTrigger] = useState(false);
 
-    const deletehandler = (e) => {   
-        setSelectedHouse(e.target.id);
-        setTrigger(true);
-    }
+  const handle = (house) => {
+    setTrigger(true);
+    dispatch(getHouse(house))
+  }
 
-    const deleteConfirmed = (e) => {
-        const url = `http://localhost:3000/houses/${e.target.value}`;
-        fetch(url, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${navbar.token}`,
-            },
+  return (
+    <div className= {navbar? 'house-list-delete active' : 'house-list-delete'}>
+      {
+        houses.map((house) => {                 
+          return (
+            <div className='delete-container' key={house.id}>
+              <img className='image-delete' src={house.image_data} alt={house.name} />
+              <div className='house-data'>
+              <h1>{house.name}</h1><hr />
+              <p>{house.description}</p>
+              <p>{house.price}</p>
+              <button className='delete-btn' id={house.id} onClick={() => handle(house)} >Delete</button>
+              < DeletePopUp trigger={trigger}  setTrigger={setTrigger} />
+              </div>
+            </div>
+          )
         })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('Success:', data);
-        }
-        )
-    
-
-    }
-
-    return (
-        <div className= {navbar? 'house-list-delete active' : 'house-list-delete'}>
-            {
-                house.map((house, index) => {                 
-                    return (
-                        <div className='delete-container' key={index}>
-                            <img className='image-delete' src={house.image_data} alt={house.name} />
-                            <div className='house-data'>
-                            <h1>{house.name}</h1><hr />
-                            <p>{house.description}</p>
-                            <p>{house.price}</p>
-                            <button className='delete-btn' id={house.id} onClick={deletehandler}>Delete</button>
-                            < DeletePopUp selectedhouse={selectedhouse}  trigger={trigger}  setTrigger={setTrigger} deleteConfirmed = {deleteConfirmed} />
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
+      }
+    </div>
+  )
 }
