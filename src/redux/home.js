570b8933ba210b2b19e8a27975237f3f119ba505
Sanja <sanja1mandic@gmail.com>
2022-11-07 01:45:48 +0100

    Add saveHome action to home reducer, for saving house to database

--- src/redux/home.js
+++ src/redux/home.js
@@ -1,41 +1,53 @@
 const url = "http://127.0.0.1:3000/houses"
-
 const GET_HOUSES = "GET_HOUSES"
-const GET_HOUSES_DETAILS = "GET_HOUSES_DETAILS"
+const POST_HOUSE = "POST_HOUSE"
 
 const initialState = []
 
-const homeReducer = (state = initialState, {type, payload}) => {
-    switch (type) {
-        case GET_HOUSES:
-        return payload 
-        case GET_HOUSES_DETAILS:
-        return payload
-        default:
-        return state;
-    }
+const homeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_HOUSES:
+      return action.payload
+    case POST_HOUSE:
+      return [...state, action.payload]
+    default:
+      return state;
+  }
 }
 
 export default homeReducer;
 
-export const getHousesDetails = (id) => async(dispatch) => { 
-    const response = await fetch(url);
-    const houses = await response.json();
-    const house = houses.find(house => house.id === id)
-    
-    dispatch({
-        type: GET_HOUSES_DETAILS,
-        payload: house
-    })
-}
-
-
 export const getHouses = () => async(dispatch) => {
-    const response = await fetch(url);
-    const houses = await response.json();
-    
-    dispatch({
-        type: GET_HOUSES,
-        payload: houses
-    })
+  const response = await fetch(url);
+  const houses = await response.json();
+
+  dispatch({
+      type: GET_HOUSES,
+      payload: houses
+  })
 }
+
+export const saveHouse = (house) => async(dispatch) => {
+  await fetch(url,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        name: house.name,
+        description: house.description,
+        image_data: house.image_data,
+        price: house.price,
+        city: house.city,
+        adress: house.adress,
+        available: house.available,
+        user_id: house.user_id
+      }),
+      headers: {
+       "Content-Type": "application/json"
+      },
+    });
+
+  dispatch({
+    type: POST_HOUSE,
+    payload: house,
+  })
+};
